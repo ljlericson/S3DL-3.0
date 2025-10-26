9f2acab561d2ff4853ae7cd0d39ca4e219baa04d
Linus Ericson <ljlericson@protonmail.com>
2025-10-27 12:29:57 +1300

    Finally fixed buildGNU still being in library

--- buildGNU/CMakeFiles/s3glr.dir/src/Application/EventManager/Event.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/s3glr.dir/src/Application/EventManager/Event.cpp.obj: \
- C:\Users\Admin\Documents\NewProjects\s3glr\src\Application\EventManager\Event.cpp
--- buildGNU/CMakeFiles/s3glr.dir/src/Application/EventManager/EventManager.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/s3glr.dir/src/Application/EventManager/EventManager.cpp.obj: \
- C:\Users\Admin\Documents\NewProjects\s3glr\src\Application\EventManager\EventManager.cpp
